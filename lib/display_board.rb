7eb1e6f63f7d3e40651f72d3c14f8996bfc9edab
Senada Kadric <skadric@fordham.edu>
2020-02-23 18:26:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,8 +2,8 @@
 # out the current state.
 def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
 
-    board[0] = "O"
-    board[4] = "X"
+    #board[0] = "O"
+    #board[4] = "X"
 
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
