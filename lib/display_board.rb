091a077ef65e76507a97495aff0daa04fd7ae911
Senada Kadric <skadric@fordham.edu>
2020-02-23 18:14:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@ def display_board(board = [" "," "," "," "," "," "," "," "," "])
 
     puts "   |   |   "
     puts "-----------"
-    puts "   |   |   "
+    puts "   | #{board[5]} |   "
     puts "-----------"
     puts "   |   |   "
 
