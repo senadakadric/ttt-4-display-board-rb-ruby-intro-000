fb2801456bfd6871b9b905365500e9de544c2ed1
Senada Kadric <skadric@fordham.edu>
2020-02-23 18:21:03 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(board = ["X","X","X"," "," "," "," "," "," "])
+def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
 
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts " #{board[0]} | #{board[3]} | #{board[6]} "
     puts "-----------"
-    puts " #{board[2]} | #{board[4]} | #{board[5]} "
+    puts " #{board[1]} | #{board[4]} | #{board[7]} "
     puts "-----------"
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+    puts " #{board[2]} | #{board[5]} | #{board[8]} "
 
 end
